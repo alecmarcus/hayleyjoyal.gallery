c17c21edbfbbaa7de4c4e65c90b4bd38ccc6854c
alecmakeus <alecmarcus.n@gmail.com>
2018-11-07 12:29:15 -0500

    Change site name, use hash mode

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,11 +2,14 @@ const pkg = require('./package')
 
 module.exports = {
   mode: 'spa',
+  router: {
+    mode: 'hash'
+  },
   /*
   ** Headers of the page
   */
   head: {
-    title: pkg.name,
+    title: 'Hayley Joyal',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
